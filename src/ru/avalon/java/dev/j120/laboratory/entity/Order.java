b9e29ea7771fe968e9e890ec40ed2917d11ed80e
buzzz <buzzz@DESKTOP-K7FJA2L>
2020-05-17 17:38:46 +0300

    initial comit

--- src/ru/avalon/java/dev/j120/laboratory/entity/Order.java
+++ src/ru/avalon/java/dev/j120/laboratory/entity/Order.java
@@ -9,43 +9,26 @@ public class Order implements Serializable {
     private LocalDate creationDateOrder;  // дата создания заказа
     private Person contactPerson;         // контактное лицо
     private String orderStatus;           // статус заказа (готовится, отгружен, отменен)
-    private Goods goodsPosition;          // список позиций товара (товар, количество, цена)
+    private ArrayList<Goods> goodsPosition;          // список позиций товара (товар, количество, цена)
 
-    public Order(LocalDate creationDateOrder, Person contactPerson, String orderStatus, Goods goodsPosition) {
+    public Order(LocalDate creationDateOrder, Person contactPerson, String orderStatus, ArrayList<Goods> goodsPosition) {
         this.creationDateOrder = creationDateOrder;
         this.contactPerson = contactPerson;
         this.orderStatus = orderStatus;
         this.goodsPosition = goodsPosition;
     }
+
     
-    public static void orderGoods(Goods g) { // заказанный товар
-        ArrayList<Goods> oGood = new ArrayList<>();
-        oGood.add(g);
-        for(int i = 0; i < oGood.size(); i++) {
-            Object elements = oGood.get(i);
-            System.out.println(elements);
-        }
-    }
     
-    public static void quantityGoods() {  // количество товара
-        
-    }
 
     public LocalDate getCreationDateOrder() {
         return creationDateOrder;
-    }
-
-    public void setCreationDateOrder(LocalDate creationDateOrder) {
-        this.creationDateOrder = creationDateOrder;
     }
 
     public Person getContactPerson() {
         return contactPerson;
     }
 
-    public void setContactPerson(Person contactPerson) {
-        this.contactPerson = contactPerson;
-    }
 
     public String getOrderStatus() {
         return orderStatus;
@@ -55,19 +38,19 @@ public class Order implements Serializable {
         this.orderStatus = orderStatus;
     }
 
-    public Goods getGoodsPosition() {
+    public ArrayList<Goods> getGoodsPosition() {
         return goodsPosition;
     }
 
-    public void setGoodsPosition(Goods goodsPosition) {
+    public void setGoodsPosition(ArrayList<Goods> goodsPosition) {
         this.goodsPosition = goodsPosition;
     }
 
     @Override
     public String toString() {
-        return "Order{" + "creationDateOrder=" + 
-                creationDateOrder + ", contactPerson=" + 
-                contactPerson + ", orderStatus=" + 
-                orderStatus + ", goodsPosition=" + goodsPosition + '}';
+        return (" Дата создания заказа: " + creationDateOrder + "\n" +
+                " Заказчик : " + contactPerson + "\n" +
+                " Статус заказа: " + orderStatus + "\n" +
+                " Список позиций: " + goodsPosition);
     }
 }
